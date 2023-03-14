d61fdd7f0bb9486ca16faabf499c75b6aa87796a
Frat Ulker <frat@cleanmob.eu>
2023-03-14 11:40:17 +0100

    add: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import Home from './pages/Home/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 
   return (
     <div className="App">
-      {/* TODO: put router here */}
-      <Home />
+      <Router>
+        <Routes>
+          <Route path='*' element={<Home />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/home' element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
