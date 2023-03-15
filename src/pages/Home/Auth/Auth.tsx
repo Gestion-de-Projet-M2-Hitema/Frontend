10d828710d255912d0861c271eac2bda0ffc4f14
f <fratulker@gmail.com>
2023-03-15 10:57:20 +0100

    add: Auth page

--- src/pages/Home/Auth/Auth.tsx
+++ src/pages/Home/Auth/Auth.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react"
+import { useDispatch } from 'react-redux';
+import { postLogin } from "../../../stores/authStore";
+import { AppDispatch } from "../../../stores/store";
 import "./style.scss"
 
 const Auth = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleLogin = () => dispatch(postLogin({email, password}))
 
     return (
         <div className="Auth">
-            AAAAAAAAAAAAAAAAA
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <input type="text" value={password} onChange={(e) => setPassword(e.target.value)} />
+            <button onClick={handleLogin}>Login</button>
         </div>
     )
 }
