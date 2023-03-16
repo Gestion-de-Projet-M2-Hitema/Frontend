5f54ac21441f31a258f4df1083fb320e2d93dc61
Micaël <micaeldupuis@live.fr>
2023-03-16 13:15:27 +0100

    MD : Add Theme Logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-import Home from './pages/Home/Home';
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Auth from './pages/Home/Auth/Auth';
+import { ThemeContext } from './context/theme.context';
 import { Provider } from "react-redux";
 import { store } from './stores/store';
 
+import Home from './pages/Home/Home';
+import "./style.scss"
 const App = () => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const changeTheme = () => {
+    if(theme === "light"){
+      setTheme("dark")
+      localStorage.setItem("theme", "dark")
+    } 
+    else{
+      setTheme("light")
+      localStorage.setItem("theme", "light")
+    }
+  }
 
   return (
     <div className="app" id={theme}>
