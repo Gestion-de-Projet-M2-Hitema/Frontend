8775abff8792926b57da0cdc192d7f0ffa46bd14
Micaël <micaeldupuis@live.fr>
2023-04-18 17:00:07 +0200

    MD : Fix auth one again

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from "axios";
+import Cookies from "js-cookie";
+
+axios.interceptors.request.use((config) => {
+  let cookie = Cookies.get("jwt")
+  if (cookie) config.headers["authorization"] = "Bearer " + cookie;
+  return config;
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
