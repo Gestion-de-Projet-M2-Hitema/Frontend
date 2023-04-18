7a32cbf7f9db411425b460a9c8372235bf38e915
f <fratulker@gmail.com>
2023-04-18 12:41:24 +0200

    add: route guard

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { ThemeContext } from './context/theme.context';
 import { Provider } from "react-redux";
 import { store } from './stores/store';
@@ -12,9 +12,11 @@ import Dashboard from "./pages/Dashboard/Dashboard";
 
 import "./style.scss"
 import Profile from "./pages/Profil/Profile";
+import Cookies from "js-cookie";
 
 const App = () => {
   const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+  const [user] = useState(Cookies.get("jwt") || "")
 
   const changeTheme = () => {
     if(theme === "light"){
@@ -38,8 +40,10 @@ const App = () => {
               <Route path='/home' element={<Home />} />
               <Route path='/signin' element={<Signin />} />
               <Route path='/signup' element={<Signup />} />
-              <Route path='/me' element={<Profile />} />
-              <Route path='/dashboard' element={<Dashboard />} />
+              <Route element={<ProtectedRoute user={user} />}>
+                <Route path='/me' element={<Profile />} />
+                <Route path='/dashboard' element={<Dashboard />} />
+              </Route>
             </Routes>
           </Router>
         </Provider>
@@ -47,5 +51,11 @@ const App = () => {
     </div>
   )
 }
+
+const ProtectedRoute = ({ user, redirectPath = '/home', children }: any) => {
+  if (!user) return <Navigate to={redirectPath} replace />;
+
+  return children;
+};
 
 export default App
