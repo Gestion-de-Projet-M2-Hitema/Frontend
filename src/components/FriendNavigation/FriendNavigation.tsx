1148e8f2bad2305ef07126c0f56ac60d811952b8
f <fratulker@gmail.com>
2023-04-20 17:29:23 +0200

    add: remove friend route

--- src/components/FriendNavigation/FriendNavigation.tsx
+++ src/components/FriendNavigation/FriendNavigation.tsx
@@ -5,23 +5,31 @@ import { useNavigate, Outlet } from "react-router-dom";
 import { TextField } from "@mui/material";
 
 import "./style.scss"
-import { Friend, getFriends } from "../../stores/friendStore";
+import { Friend, deleteFriends, getFriends } from "../../stores/friendStore";
+import CloseIcon from "../../assets/icons/CloseIcon";
 
 const FriendNavigation = () => {
 	const navigate = useNavigate()
 	const dispatch = useDispatch<AppDispatch>();
 	const { friendList } = useSelector((state: RootState) => state.friend)
 
+	const handleFriendRemove = (id: string) => {
+		dispatch(deleteFriends({id}))
+	}
+
 	useEffect(() => {
 	  dispatch(getFriends())
 	}, [])
   
 	return (
-		<div id="serverNavigation">
+		<div id="friend-navigation">
 			<h1>Friends</h1>
 			<ul>
 				{friendList && friendList.map((item: Friend) => (
-					<li key={item.id}>{item.username}</li>
+					<li key={item.id}>
+						<span>{item.username}</span>
+						<span className="remove-friend" onClick={() => handleFriendRemove(item.id)}><CloseIcon /></span>
+					</li>
 				))}
 			</ul>
 		</div>
