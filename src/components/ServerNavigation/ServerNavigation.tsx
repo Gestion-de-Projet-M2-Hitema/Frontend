d744f7f3802f8bf21a09cd7e7911bdb686a086d4
Frat Ulker <frat@cleanmob.eu>
2023-06-13 11:10:40 +0200

    remove: duplicate code

--- src/components/ServerNavigation/ServerNavigation.tsx
+++ src/components/ServerNavigation/ServerNavigation.tsx
@@ -128,21 +128,6 @@ const ServerNavigation = () => {
 					})}
 				</div>
 				
-				<div className="channels">
-					<h3>Users</h3>
-					{channelList.map(e => {
-						return (
-							<div key={e.id} className={`itemChannel ${selected == 'channel-' + e.id && 'channelActive'}`}  onClick={() => {
-								setSelected("channel-" + e.id)
-								handleChannelSelection(e.id as string)
-							}}>
-								<ChannelIcon />
-								<h4>{e.name}</h4>
-							</div>
-						)
-					})}
-				</div>
-
 				<Modal
 					open={open}
 					onClose={() => handleClose()}
